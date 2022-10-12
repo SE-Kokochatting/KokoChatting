9aa34680d5c56bc488e7d0eb53b6501ea81bf3b6
DanmoSAMA <772523546@qq.com>
2022-10-12 11:22:30 +0800

    ✨ feat(fe): Chat Window - sender
    Add the chat window. Currently only finish the sender.

---
+++ src/frontend/src/components/ChatWindow/index.tsx
@@ -0,0 +1,13 @@
+import Sender from './components/Sender'
+import './index.scss'
+// type ChatWindowProps = {
+// };
+function ChatWindow(/* props: ChatWindowProps */) {
+  // const {} = props;
+  return (
+    <div className='c-chat_window'>
+      <Sender />
+    </div>
+  )
+}
+export default ChatWindow
