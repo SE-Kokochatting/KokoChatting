0d112a81f6b0e81eefb0519f6da70d5c4554c31f
DanmoSAMA <772523546@qq.com>
2022-10-13 11:06:47 +0800

    ✨ feat(fe): Chat bubbles.
    Add chat bubbles.

--- src/frontend/src/components/ChatWindow/index.tsx
+++ src/frontend/src/components/ChatWindow/index.tsx
@@ -1,4 +1,6 @@
+import Bubble from './components/Bubble'
 import Sender from './components/Sender'
+import { Direction } from '@/enums'
 import './index.scss'
 // type ChatWindowProps = {
 // };
@@ -6,6 +8,24 @@ function ChatWindow(/* props: ChatWindowProps */) {
   // const {} = props;
   return (
     <div className='c-chat_window'>
+      <div className='c-chat_window-chat_area'>
+        <Bubble
+          content='70周年校庆快乐！'
+          direction={Direction.Left}
+          time='8:01'
+        />
+        <Bubble
+          content='同乐！'
+          direction={Direction.Right}
+          read={true}
+          time='8:02'
+        />
+        <Bubble
+          content='70周年校庆快乐！'
+          direction={Direction.Left}
+          time='8:03'
+        />
+      </div>
       <Sender />
     </div>
   )
