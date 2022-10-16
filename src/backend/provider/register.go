0e61968b3dd32b8a4362ead3447cd9e55d3f4a10
lingwu <lingwu_hb@163.com>
2022-10-16 15:59:34 +0800

    feat: finish user register

--- src/backend/provider/register.go
+++ src/backend/provider/register.go
@@ -1,11 +1,36 @@
 package provider
 
+import (
+	"KokoChatting/global"
+	"github.com/jinzhu/gorm"
+	"go.uber.org/zap"
+)
+
 type RegisterProvider struct {
 	mysqlProvider
 }
 
-// 操作数据库表函数
+// AddUser 增加用户到用户表中
+func (regPro *RegisterProvider) AddUser(name string, encrytedpassword string) (uint64, error) {
+	// 查找有无重复的name用户
+	var userProfileEntity = &storeUserProfile{}
+	dbClient := regPro.mysqlProvider.mysqlDb
+	if err := dbClient.Where("name = ?", name).First(userProfileEntity).Error; err != nil {
+		if err != gorm.ErrRecordNotFound {
+			global.Logger.Error("name is already used", zap.Error(err))
+			return 0, err
+		}
+	}
 
+	// 添加到数据表中
+	userProfileEntity.Name = name
+	userProfileEntity.Password = encrytedpassword
+	if err := dbClient.Create(userProfileEntity).Error; err != nil {
+		global.Logger.Error("add user error", zap.Error(err))
+		return 0, err
+	}
+	return userProfileEntity.Uid+100000, nil
+}
 
 func NewRegisterProvider() *RegisterProvider {
 	return &RegisterProvider{
--- src/backend/service/register.go
+++ src/backend/service/register.go
@@ -4,6 +4,7 @@ import (
 	"KokoChatting/global"
 	"KokoChatting/provider"
 	"KokoChatting/util"
+	"go.uber.org/zap"
 )
 
 type RegisterService struct {
@@ -13,7 +14,15 @@ type RegisterService struct {
 // Register 用户注册接口
 func (regSrv *RegisterService) Register(name string, password string) (uint64, error) {
 	encryptedPassword := util.Encryption(password)
+	// 将该用户添加到数据库表中，然后返回该用户表中的uid
+	// 同时进行检查该用户是否可以添加到数据库中（查重）
+	uid, err := regSrv.registerProver.AddUser(name, encryptedPassword)
+	if err != nil {
+		global.Logger.Error("add user error", zap.Error(err))
+		return 0, err
+	}
 
+	return uid, err
 }
 
 func NewRegisterService() *RegisterService {
